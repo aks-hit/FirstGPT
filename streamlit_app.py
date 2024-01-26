462ccf0ff69a6739a030f9087f48bb9cc96442fd
Akshit Singh <akshit.singh.cse25@heritageit.edu.in>
2024-01-27 03:40:02 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,44 @@
+# Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
+
+from openai import OpenAI
+import streamlit as st
+
+client = OpenAI(key=st.secrets['OPENAI_API_KEY'],
+api_key=st.text_input('Enter OpenAI API token:', type='password'))
+
+
+# Developer's api key is in secrets file.
+# dev_api_key = st.secrets['OPENAI_API_KEY']
+
+# Ask the user's api key
+with st.sidebar:
+    st.title('🤖💬 OpenAI Chatbot')
+    if 'OPENAI_API_KEY' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+    else:
+        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(model="gpt-3.5-turbo",
+        messages=[{"role": m["role"], "content": m["content"]}
+                  for m in st.session_state.messages], stream=True):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
