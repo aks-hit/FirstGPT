84d21dcd38f96b7d688550085f8bec9fc1669b22
Akshit Singh <akshit.singh.cse25@heritageit.edu.in>
2024-01-27 12:49:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,22 +4,40 @@ from openai import OpenAI
 import streamlit as st
 
 #client = OpenAI(key=st.secrets['OPENAI_API_KEY'],
-api_key=st.text_input('Enter OpenAI API token:', type='password')
+#api_key=st.text_input('Enter OpenAI API token:', type='password')
+
+
+# Developer's api key is in secrets file.
+# dev_api_key = st.secrets['OPENAI_API_KEY']
+import streamlit as st
+from openai import OpenAI
 
 
 # Developer's api key is in secrets file.
 # dev_api_key = st.secrets['OPENAI_API_KEY']
 
 # Ask the user's api key
-with st.sidebar:
-    st.title('🤖💬 OpenAI Chatbot')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
+with st.form('form'):
+    user_api_key = st.text_input('Enter OpenAI API token:', type='password')
+    submit = st.form_submit_button('Submit')
+
+if submit:
+    if len(user_api_key) == 51 and user_api_key.startswith('sk-'):
+        client = OpenAI(api_key=user_api_key)
+        st.success('api key is successfully entered')
     else:
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
+        st.error('Your api key is invalid.')
+        st.stop()
+# Ask the user's api key
+# with st.sidebar:
+#    st.title('🤖💬 OpenAI Chatbot')
+#   if 'OPENAI_API_KEY' in st.secrets:
+#       st.success('API key already provided!', icon='✅')
+#   else:
+#       if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+#           st.warning('Please enter your credentials!', icon='⚠️')
+#       else:
+#           st.success('Proceed to entering your prompt message!', icon='👉')
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
